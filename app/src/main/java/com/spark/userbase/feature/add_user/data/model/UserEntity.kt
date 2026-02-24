d05e492ca6ea384e1757d275708ce0f4c9e184f5
Gesraha <ahmed.k.gesraha@gmail.com>
2026-02-24 08:36:25 +0200

    Added add user domain layer with models mapping

--- app/src/main/java/com/spark/userbase/feature/add_user/data/model/UserEntity.kt
+++ app/src/main/java/com/spark/userbase/feature/add_user/data/model/UserEntity.kt
@@ -2,6 +2,7 @@ package com.spark.userbase.feature.add_user.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.spark.userbase.feature.add_user.domain.model.User
 
 @Entity(tableName = "users")
 data class UserEntity(
@@ -10,4 +11,20 @@ data class UserEntity(
     val age: Int,
     val jobTitle: String,
     val gender: String,
+)
+
+fun UserEntity.toDomain() = User(
+    id = id,
+    name = name,
+    age = age,
+    jobTitle = jobTitle,
+    gender = gender,
+)
+
+fun User.toEntity() = UserEntity(
+    id = id,
+    name = name,
+    age = age,
+    jobTitle = jobTitle,
+    gender = gender,
 )
